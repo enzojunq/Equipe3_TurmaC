1a7a5f724f17c7d5f118e1f34ce6dd52ee3bb614
Enzo Junqueira <enzopj@al.insper.edu.br>
2023-10-17 23:16:37 -0300

    locais por perto e feed menu

--- js/popup.js
+++ js/popup.js
@@ -5,4 +5,12 @@ function openPopup() {
   function closePopup() {
     document.getElementById("popup").style.display = "none";
   }
+
+function openPopup2() {
+    document.getElementById("popup2").style.display = "block";
+  }
   
+  function closePopup2() {
+    document.getElementById("popup2").style.display = "none";
+  }
+
